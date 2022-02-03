c52f8355d5d5e7a2999c50f0fa553597b5a232de
Kartik Ohri <kartikohri13@gmail.com>
2022-02-03 13:35:31 +0530

    simplify stuff

--- recording_similarity_import.py
+++ recording_similarity_import.py
@@ -2,7 +2,6 @@
 
 import csv
 import glob
-import os.path
 
 import psycopg2
 from psycopg2.extras import DictCursor, execute_values
@@ -15,15 +14,14 @@ with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
         with mb_conn.cursor(cursor_factory=DictCursor) as mb_curs:
             table = f"mapping.recording_similarity_index_{size}"
             mb_curs.execute(f"""
-                CREATE TABLE mapping.recording_similarity_index_{size} (
+                CREATE TABLE IF NOT EXISTS mapping.recording_similarity_index_{size} (
                     mbid0           UUID NOT NULL,
                     mbid1           UUID NOT NULL,
                     similarity      DOUBLE PRECISION NOT NULL
                 )
             """)
 
-            directory = f"~/recording_similarity_index/{size}"
-            for path in glob.glob(os.path.join(directory, "*.csv")):
+            for path in glob.glob(f"/home/lucifer/recording_similarity_index_{size}/*.csv"):
                 with open(path, newline='') as csvfile:
                     index = csv.reader(csvfile)
                     values = (tuple(row) for row in index)
