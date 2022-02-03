4ca41c88a9dc9216ee463247f6fb8cd1c5e25af3
Kartik Ohri <kartikohri13@gmail.com>
2022-02-03 13:26:30 +0530

    fix script

--- recording_similarity_import.py
+++ recording_similarity_import.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import csv
 import glob
 import os.path
@@ -8,32 +10,27 @@ from psycopg2.extras import DictCursor, execute_values
 from listenbrainz import config
 
 
-def import_recording_similarity():
-    with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
-        for size in [1, 5, 10]:
-            with mb_conn.cursor(cursor_factory=DictCursor) as mb_curs:
-                table = f"mapping.recording_similarity_index_{size}"
-                mb_curs.execute(f"""
-                    CREATE TABLE mapping.recording_similarity_index_{size} (
-                        mbid0           UUID NOT NULL,
-                        mbid1           UUID NOT NULL,
-                        similarity      DOUBLE PRECISION NOT NULL
-                    )
-                """)
+with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
+    for size in [1, 5, 10]:
+        with mb_conn.cursor(cursor_factory=DictCursor) as mb_curs:
+            table = f"mapping.recording_similarity_index_{size}"
+            mb_curs.execute(f"""
+                CREATE TABLE mapping.recording_similarity_index_{size} (
+                    mbid0           UUID NOT NULL,
+                    mbid1           UUID NOT NULL,
+                    similarity      DOUBLE PRECISION NOT NULL
+                )
+            """)
 
-                directory = f"~/recording_similarity_index/{size}"
-                for path in glob.glob(os.path.join(directory, "*.csv")):
-                    with open(path, newline='') as csvfile:
-                        index = csv.reader(csvfile)
-                        values = (tuple(row) for row in index)
+            directory = f"~/recording_similarity_index/{size}"
+            for path in glob.glob(os.path.join(directory, "*.csv")):
+                with open(path, newline='') as csvfile:
+                    index = csv.reader(csvfile)
+                    values = (tuple(row) for row in index)
 
-                        query = f"INSERT INTO {table} VALUES %s"
-                        template = "(%s::UUID, %s::UUID, %s)"
+                    query = f"INSERT INTO {table} VALUES %s"
+                    template = "(%s::UUID, %s::UUID, %s)"
 
-                        execute_values(mb_curs, query, values, template)
+                    execute_values(mb_curs, query, values, template)
 
-                mb_conn.commit()
-
-
-if __name__ == '__main__':
-    import_recording_similarity()
+            mb_conn.commit()
