f3e71ee6b5cf4bb4169b3cef228f7a8c3b43a012
Kartik Ohri <kartikohri13@gmail.com>
2022-02-09 12:35:58 +0530

    Add artist similarity index import

--- similarity_index_import.py
+++ similarity_index_import.py
@@ -2,6 +2,7 @@
 
 import csv
 import glob
+import sys
 
 import psycopg2
 from psycopg2.extras import DictCursor, execute_values
@@ -9,19 +10,19 @@ from psycopg2.extras import DictCursor, execute_values
 from listenbrainz import config
 
 
-with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
-    for size in [1, 5, 10]:
+def import_index(entity: str, size: int):
+    with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
         with mb_conn.cursor(cursor_factory=DictCursor) as mb_curs:
-            table = f"mapping.recording_similarity_index_{size}"
+            table = f"mapping.{entity}_similarity_index_{size}"
             mb_curs.execute(f"""
-                CREATE TABLE IF NOT EXISTS mapping.recording_similarity_index_{size} (
+                CREATE TABLE IF NOT EXISTS mapping.{entity}_similarity_index_{size} (
                     mbid0           UUID NOT NULL,
                     mbid1           UUID NOT NULL,
                     similarity      DOUBLE PRECISION NOT NULL
                 )
             """)
 
-            for path in glob.glob(f"/home/lucifer/recording_similarity_index_{size}/*.csv"):
+            for path in glob.glob(f"/home/lucifer/{entity}_similarity_index_{size}/*.csv"):
                 with open(path, newline='') as csvfile:
                     index = csv.reader(csvfile)
                     values = (tuple(row) for row in index)
@@ -32,3 +33,10 @@ with psycopg2.connect(config.MBID_MAPPING_DATABASE_URI) as mb_conn:
                     execute_values(mb_curs, query, values, template)
 
             mb_conn.commit()
+
+
+if __name__ == '__main__':
+    _entity, _size = sys.argv[1], int(sys.argv[2])
+    print(f"Importing index for {_entity} of window size {_size}")
+    import_index(_entity, _size)
+    print("Import complete.")
